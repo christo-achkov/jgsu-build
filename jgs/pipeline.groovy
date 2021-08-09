fdca3bb5d2b2c31ac6ff35270072df93cb6108f0
zaibatsu-loli <christo.achkov@gmail.com>
2021-08-10 01:29:30 +0300

    feat: sample config to test reading config from repo

---
+++ jgs/pipeline.groovy
@@ -0,0 +1,31 @@
+pipeline {
+    agent any
+
+    // tools {
+    //     // Install the Maven version configured as "M3" and add it to the path.
+    //     maven "M3"
+    // }
+
+    stages {
+        stage('Checkout') {
+            steps {
+                // Get some code from a GitHub repository
+                git branch: 'main', url: 'https://github.com/zaibatsu-loli/jgsu-spring-petclinic'
+            }
+        }
+        stage('Build') {
+            steps {
+                sh './mvnw clean compile'
+            }
+
+            post {
+                // If Maven was able to run the tests, even if some of the test
+                // failed, record the test results and archive the jar file.
+                success {
+                    junit '**/target/surefire-reports/TEST-*.xml'
+                    archiveArtifacts 'target/*.jar'
+                }
+            }
+        }
+    }
+}
